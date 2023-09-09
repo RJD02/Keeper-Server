0d871828833e4f71a5c370c8f75716511f4355f7
RJD02 <dulangeraviraj@gmail.com>
2023-09-09 09:43:31 +0530

    Add mongo db url

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -5,24 +6,24 @@ const routes = require("./routes/routes");
 const app = express();
 
 mongoose
-  .connect("mongodb://localhost:27017/KeepersDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("MONGO CONNECTION OPEN");
-  })
-  .catch((err) => {
-    console.log("Oh no! Mongo connection error");
-    console.log(err);
-  });
+    .connect(process.env.ONNECTION_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN");
+    })
+    .catch((err) => {
+        console.log("Oh no! Mongo connection error");
+        console.log(err);
+    });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: "http://localhost:3000/",
-  optionsSuccessStatus: 200,
+    origin: "http://localhost:3000/",
+    optionsSuccessStatus: 200,
 };
 
 app.use(cors());
@@ -30,9 +31,9 @@ app.use(cors());
 app.use("/api", routes);
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+    res.send("Hello world");
 });
 
 app.listen(9000, (req, res) => {
-  console.log("Listening on port 9000");
+    console.log("Listening on port 9000");
 });
